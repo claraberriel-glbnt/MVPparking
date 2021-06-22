c1b7a92b1e6e00e1a27521f301e69813d239f4b4
María Clara Berriel <maria.berriel@globant.com>
2021-06-22 14:26:33 -0300

    exception test fixed

--- app/src/test/java/com/claraberriel/mvpparking/mvp/presenter/ParkingSizePresenterTest.java
+++ app/src/test/java/com/claraberriel/mvpparking/mvp/presenter/ParkingSizePresenterTest.java
@@ -1,30 +1,41 @@
 package com.claraberriel.mvpparking.mvp.presenter;
 
+import android.util.Log;
+
 import com.claraberriel.mvpparking.mvp.model.ParkingModel;
 import com.claraberriel.mvpparking.mvp.view.ParkingSizeView;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.MockedStatic;
 
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.mockStatic;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+
 
 public class ParkingSizePresenterTest {
     private ParkingSizePresenter presenter;
     private ParkingModel model;
     private ParkingSizeView view;
+    private MockedStatic<Log> logMockedStatic;
 
     @Before
     public void setUp() {
         model = mock(ParkingModel.class);
         view = mock(ParkingSizeView.class);
         presenter = new ParkingSizePresenter(model, view);
+        logMockedStatic = mockStatic(Log.class);
     }
 
     @Test
     public void onSumbitClicked() {
-        // Given certain size
+        // Given certain valid size
         when(view.getParkingSize()).thenReturn(10);
         // When Submit button is pressed
         presenter.onSubmit();
@@ -33,4 +44,20 @@ public class ParkingSizePresenterTest {
         verify(model).setParkingSize(10);
     }
 
+    @Test
+    public void onSubmitClickedException() {
+        // Given an out of range situation
+        when(view.getParkingSize()).thenReturn(128);
+        doThrow(new NumberFormatException()).when(model).setParkingSize(128);
+        // When Submit button is pressed
+        presenter.onSubmit();
+        // Then throw error message
+        verify(view).showInvalidError();
+        verify(view, never()).showParkAmount(anyInt());
+    }
+
+    @After
+    public void tearDown() {
+        logMockedStatic.close();
+    }
 }
