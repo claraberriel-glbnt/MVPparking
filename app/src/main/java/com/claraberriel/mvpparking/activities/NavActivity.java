d833f7379ea11b44124b29f753393002e68f4a78
María Clara Berriel <maria.berriel@globant.com>
2021-06-25 10:54:12 -0300

    navigation between activities and setting fragment classes

--- app/src/main/java/com/claraberriel/mvpparking/activities/NavActivity.java
+++ app/src/main/java/com/claraberriel/mvpparking/activities/NavActivity.java
@@ -1,6 +1,16 @@
 package com.claraberriel.mvpparking.activities;
 
 import android.app.Activity;
+import android.os.Bundle;
+
+import com.claraberriel.mvpparking.databinding.ActivityNavigationBinding;
 
 public class NavActivity extends Activity {
+    private ActivityNavigationBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityNavigationBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+    }
 }
--- app/src/main/java/com/claraberriel/mvpparking/activities/ReserveFragment.java
+++
@@ -1,6 +0,0 @@
-package com.claraberriel.mvpparking.activities;
-
-import android.app.Activity;
-
-public class ReserveFragment extends Activity {
-}
