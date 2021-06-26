541fc771b5364597d4251c1e80fcf6f355d171e7
María Clara Berriel <maria.berriel@globant.com>
2021-06-26 09:55:43 -0300

    done with navigation

--- app/src/main/java/com/claraberriel/mvpparking/activities/NavActivity.java
+++ app/src/main/java/com/claraberriel/mvpparking/activities/NavActivity.java
@@ -7,14 +7,17 @@ import androidx.fragment.app.FragmentTransaction;
 
 import com.claraberriel.mvpparking.R;
 import com.claraberriel.mvpparking.databinding.ActivityNavigationBinding;
+import com.claraberriel.mvpparking.fragments.AutoReleaseFragment;
 import com.claraberriel.mvpparking.fragments.ReleaseFragment;
 import com.claraberriel.mvpparking.fragments.ReserveFragment;
 
 public class NavActivity extends AppCompatActivity {
 
+    private ActivityNavigationBinding binding;
     private static final String RESERVE_FRAGMENT_TAG = "RESERVE_FRAGMENT";
     private static final String RELEASE_FRAGMENT_TAG = "RELEASE_FRAGMENT";
-    private ActivityNavigationBinding binding;
+    private static final String AUTO_RELEASE_FRAGMENT_TAG = "AUTO_RELEASE_FRAGMENT";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,15 +31,21 @@ public class NavActivity extends AppCompatActivity {
 
     public void setListeners() {
         binding.buttonReservation.setOnClickListener(view -> {
-            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-            transaction.replace(R.id.fragment_container, new ReserveFragment(), RESERVE_FRAGMENT_TAG);
-            transaction.commit();
+            FragmentTransaction transactionReserve = getSupportFragmentManager().beginTransaction();
+            transactionReserve.replace(R.id.fragment_container, new ReserveFragment(), RESERVE_FRAGMENT_TAG);
+            transactionReserve.commit();
         });
 
         binding.buttonRelease.setOnClickListener(view -> {
-            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-            transaction.replace(R.id.fragment_container, new ReleaseFragment(), RELEASE_FRAGMENT_TAG);
-            transaction.commit();
+            FragmentTransaction transactionRelease = getSupportFragmentManager().beginTransaction();
+            transactionRelease.replace(R.id.fragment_container, new ReleaseFragment(), RELEASE_FRAGMENT_TAG);
+            transactionRelease.commit();
+        });
+
+        binding.buttonAutomaticRelease.setOnClickListener(view -> {
+            FragmentTransaction transactionAuto = getSupportFragmentManager().beginTransaction();
+            transactionAuto.replace(R.id.fragment_container, new AutoReleaseFragment(), AUTO_RELEASE_FRAGMENT_TAG);
+            transactionAuto.commit();
         });
     }
 }
