c0abe127013831b40a0c22870d72886c5c81befb
María Clara Berriel <maria.berriel@globant.com>
2021-06-26 09:55:43 -0300

    done with navigation

--- app/src/main/java/com/claraberriel/mvpparking/fragments/AutoReleaseFragment.java
+++ app/src/main/java/com/claraberriel/mvpparking/fragments/AutoReleaseFragment.java
@@ -11,9 +11,9 @@ import androidx.fragment.app.Fragment;
 
 import com.claraberriel.mvpparking.R;
 
-public class ReleaseFragment extends Fragment {
+public class AutoReleaseFragment extends Fragment {
 
-    public ReleaseFragment() {
+    public AutoReleaseFragment(){
     }
 
     @Override
@@ -24,7 +24,7 @@ public class ReleaseFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_release, container, false);
+        View view = inflater.inflate(R.layout.fragment_auto_release, container, false);
         return view;
     }
 
